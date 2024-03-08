86c0fce4797d5cac28d894106a7ec0245e66ed2c
Francisco Ceballos <104146989+FranCeballos@users.noreply.github.com>
2024-03-08 20:44:35 -0300

    Fix. High Idle Time

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 
 export async function connectToDatabase() {
-  const client = await MongoClient.connect(process.env.MONGO_URL);
+  const client = await MongoClient.connect(process.env.MONGO_URL, {
+    maxIdleTimeMS: 60000,
+  });
   return client;
 }
